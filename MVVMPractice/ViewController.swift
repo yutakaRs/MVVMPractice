75c6915089498db6f57016497c1ac286527a7bf3
yUtaR <wsps70732@yahoo.com.tw>
2023-10-31 17:23:08 +0800

    1031Test commit

--- MVVMPractice/ViewController.swift
+++ MVVMPractice/ViewController.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    
     //Hi
-
+    
+    func getDataFromGit(){
+        let url = URL(string: "")
+    }
+    
 }
-
